d14ef294df45d26e3514adbe1d9733fa47e03ac0
kwangpyo <kkp0639@gmail.com>
2023-03-31 09:26:51 +0900

    [BOJ]휴게소 세우기/G4/30m

---
+++ Pangpyo/[BOJ]-1477.py
@@ -0,0 +1,24 @@
+from heapq import heappop, heappush
+import sys
+
+input = sys.stdin.readline
+
+N, M, L = map(int, input().split())
+
+nums = sorted([0] + list(map(int, input().split())) + [L])
+
+heap = []
+
+for i in range(1, N+2) :
+    heappush(heap, (-(nums[i]-nums[i-1]), (nums[i]-nums[i-1]),0))
+
+
+while M:
+    l, ol, cnt = heappop(heap)
+    cnt += 1
+    M -= 1
+    nl = -ol//(cnt+1)
+    heappush(heap, (nl, ol, cnt))
+
+
+print(-heap[0][0])
